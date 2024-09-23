792cef7046bb62898df00a91cf4ce193216834f3
Al Miha <contact.almiha@gmail.com>
2024-09-23 15:37:23 +0200

    55.Display Featured Posts On the Homepage

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,12 +1,15 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
-from blogs.models import Category
+from blogs.models import Category,Blog
 
 def home(request):
     # return HttpResponse('<h2>Homepage</h2>')
     categories=Category.objects.all()
+    featured_posts=Blog.objects.filter(is_featured=True).order_by('-updated_at')
     # print(categories)
+    # print(featured_posts)
     context={
         'categories':categories,
+        'featured_posts':featured_posts,
     }
     return render(request,'home.html',context)
