88be94c04060655304284027d81541de49a3310f
Al Miha <contact.almiha@gmail.com>
2024-09-23 11:08:20 +0200

    54.Display Categories On the Homepage

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,12 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+from blogs.models import Category
 
 def home(request):
     # return HttpResponse('<h2>Homepage</h2>')
-    return render(request,'home.html')
+    categories=Category.objects.all()
+    # print(categories)
+    context={
+        'categories':categories,
+    }
+    return render(request,'home.html',context)
