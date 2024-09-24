150b913023b0b2bf61f7c777dc2edce2b85d4f24
Al Miha <contact.almiha@gmail.com>
2024-09-24 11:40:46 +0200

    58.Posts By Category

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+# from blogs.models import Blog
+from .models import Blog
 
 # Create your views here.
+
+def posts_by_category(request, category_id):
+    # print(category_id)
+    # return HttpResponse('Posts by category')
+    # Fetch the posts that below to category with the id category_id
+    posts = Blog.objects.filter(status="Published",category=category_id)
+    context={
+        'posts':posts,
+    }
+    return render(request,'posts_by_category.html',context)
