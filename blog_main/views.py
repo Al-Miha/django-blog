1972251050ab3059b2750583bc880d8acf85471b
Al Miha <contact.almiha@gmail.com>
2024-09-24 10:34:09 +0200

    56.Display Recent Articles On Homepage

--- blog_main/views.py
+++ blog_main/views.py
@@ -5,11 +5,15 @@ from blogs.models import Category,Blog
 def home(request):
     # return HttpResponse('<h2>Homepage</h2>')
     categories=Category.objects.all()
-    featured_posts=Blog.objects.filter(is_featured=True).order_by('-updated_at')
+    featured_posts=Blog.objects.filter(is_featured=True,status='Published').order_by('-updated_at')
     # print(categories)
     # print(featured_posts)
+    posts=Blog.objects.filter(is_featured=False,status='Published')
+    # print(posts)
+
     context={
         'categories':categories,
         'featured_posts':featured_posts,
+        'posts':posts,
     }
     return render(request,'home.html',context)
