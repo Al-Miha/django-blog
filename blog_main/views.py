ad4e15dc848431d7a32f49d2e5f25359aaa693c9
Al Miha <contact.almiha@gmail.com>
2024-09-24 13:16:21 +0200

    62.Context Processors

--- blog_main/views.py
+++ blog_main/views.py
@@ -4,7 +4,8 @@ from blogs.models import Category,Blog
 
 def home(request):
     # return HttpResponse('<h2>Homepage</h2>')
-    categories=Category.objects.all()
+    #  !!! Categorie are now availble through context_processors !!!
+    # categories=Category.objects.all()
     featured_posts=Blog.objects.filter(is_featured=True,status='Published').order_by('-updated_at')
     # print(categories)
     # print(featured_posts)
@@ -12,7 +13,7 @@ def home(request):
     # print(posts)
 
     context={
-        'categories':categories,
+        # 'categories':categories,
         'featured_posts':featured_posts,
         'posts':posts,
     }
