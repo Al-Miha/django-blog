02b3b582e30a8a2bfcec63af638890a12f2cfa1f
Al Miha <contact.almiha@gmail.com>
2024-09-24 12:21:20 +0200

    59.Correcting try/except

--- blogs/views.py
+++ blogs/views.py
@@ -11,14 +11,13 @@ def posts_by_category(request, category_id):
     # Fetch the posts that below to category with the id category_id
     posts = Blog.objects.filter(status="Published",category=category_id)
 
-    # Category.objects.get(pk=category_id)
     
     # Use get object_or_404 when you want to show 404 error page if the category does not exist 
     # category= get_object_or_404(Category,pk=category_id) 
     
     #  Use try/except when we want to do some custom action if the category does not exist
     try:
-        category= get_object_or_404(Category,pk=category_id)
+        category= Category.objects.get(pk=category_id)
     except:
         # redirect the user to homepage
         return redirect('home') 
