96ea6e269aeb2524d26a43826357beccaa8477b5
Al Miha <contact.almiha@gmail.com>
2024-09-28 10:35:56 +0200

    106.EditUserForm functionality

--- dashboards/views.py
+++ dashboards/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from blogs.models import Category,Blog
 from django.contrib.auth.decorators import login_required
 
-from . forms import CategoryForm,BlogPostForm,AddUserForm
+from . forms import CategoryForm,BlogPostForm,AddUserForm,EditUserForm
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 
@@ -123,3 +123,16 @@ def add_user(request):
         'form': form,
     }
     return render(request, 'dashboard/add_user.html', context)
+
+def edit_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == 'POST':
+        form = EditUserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+    form = EditUserForm(instance=user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'dashboard/edit_user.html', context)
